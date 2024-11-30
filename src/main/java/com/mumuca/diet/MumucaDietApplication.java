73a34af050823568d6c6c06502c0675c7f9cf221
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2024-11-30 13:49:14 -0300

    Initial Commit

---
+++ src/main/java/com/mumuca/diet/MumucaDietApplication.java
@@ -0,0 +1,13 @@
+package com.mumuca.diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumucaDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumucaDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mumuca/diet/MumucaDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumuca.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumucaDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
