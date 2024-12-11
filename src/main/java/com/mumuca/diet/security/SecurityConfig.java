43dd028c6b530342ca4ccd21f470744fa7cbeaa7
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2024-12-11 20:31:41 -0300

    add filter to validate user in jwt

--- src/main/java/com/mumuca/diet/security/SecurityConfig.java
+++ src/main/java/com/mumuca/diet/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.mumuca.diet.security;
 
+import com.mumuca.diet.security.filter.UserValidationFilter;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPrivateKey;
@@ -26,6 +29,7 @@ import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Value("${jwt.public.key}")
@@ -37,14 +41,21 @@ public class SecurityConfig {
     @Value("${bcrypt.salt}")
     private int bcryptSalt;
 
+    private final UserValidationFilter userValidationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterAfter(userValidationFilter, BearerTokenAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request ->
                         request
-                                .requestMatchers("/api/v1/auth/**", "/api/v1/calculator/**", "/h2-console/**").permitAll()
+                                .requestMatchers(
+                                        "/api/v1/auth/**",
+                                        "/api/v1/calculator/**",
+                                        "/h2-console/**"
+                                ).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
