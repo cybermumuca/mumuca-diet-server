63c654754b9d3eab811f47806720dd356b342462
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2024-12-11 18:24:20 -0300

    add interceptor log to request/response

---
+++ src/main/java/com/mumuca/diet/interceptor/LoggingInterceptor.java
@@ -0,0 +1,41 @@
+package com.mumuca.diet.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final String START_TIME_ATTRIBUTE = "startTime";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Incoming Request: {} {}", request.getMethod(), request.getRequestURI());
+
+        request.setAttribute(START_TIME_ATTRIBUTE, System.currentTimeMillis());
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        Long startTime = (Long) request.getAttribute(START_TIME_ATTRIBUTE);
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        log.info("Outgoing Response: {} {} [{}] ({}ms)",
+                request.getMethod(),
+                request.getRequestURI(),
+                HttpStatus.valueOf(response.getStatus()),
+                duration
+        );
+    }
+
+
+}
